4e55c5e464ec27dda55743dc336b6bb7c7c89465
e205 <e205@e205>
2016-08-12 21:23:16 -0500

    Se hace refactor de algunas caracteristicas en metodos para girarDerecha, girarIzquierda y avanzar se crea una prueba que evalua el giro y falla.

--- Marte/src/test/java/co/edu/unbosque/TestControlArchivo.java
+++ Marte/src/test/java/co/edu/unbosque/TestControlArchivo.java
@@ -3,14 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package co.edu.unbosque;
 
-
 import co.edu.unbosque.marte.ExploracionMarte;
+import co.edu.unbosque.marte.Explorador;
 import co.edu.unbosque.marte.IExplorador;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.Assert;
 import org.testng.annotations.Test;
 
@@ -19,40 +20,59 @@ import org.testng.annotations.Test;
  * @author F211
  */
 public class TestControlArchivo {
-    
-    
+
     @Test
-    public void elArchivoExiste() throws FileNotFoundException{
-        ExploracionMarte exploracionMarte=ExploracionMarte.getInstancia();        
+    public void elArchivoExiste() throws FileNotFoundException {
+        ExploracionMarte exploracionMarte = ExploracionMarte.getInstancia();
     }
+
     /**
      * El metodo prueba que el tamaño de Marte se haya cargodo correctamente.
+     *
      * @throws FileNotFoundException
-     * @throws IOException 
+     * @throws IOException
      */
-	@Test
-	public void testCargaTamanoCorrectoMarte()throws FileNotFoundException, IOException{
-			ExploracionMarte exploracionMarte=ExploracionMarte.getInstancia(); 
-			Integer filas, columnas;
-			exploracionMarte.leerMundo();
-                         filas = exploracionMarte.getFilas();
-                        columnas = exploracionMarte.getColumnas();
-                        Assert.assertTrue(filas.equals(5));
-                        Assert.assertTrue(columnas.equals(5));
-	}
-        /**
+    @Test
+    public void testCargaTamanoCorrectoMarte() throws FileNotFoundException, IOException {
+        ExploracionMarte exploracionMarte = ExploracionMarte.getInstancia();
+        Integer filas, columnas;
+        exploracionMarte.leerMundo();
+        filas = exploracionMarte.getFilas();
+        columnas = exploracionMarte.getColumnas();
+        Assert.assertTrue(filas.equals(5));
+        Assert.assertTrue(columnas.equals(5));
+    }
+
+    /**
      * El metodo prueba que la carga del primer explorador sea correcta
+     *
      * @throws FileNotFoundException
-     * @throws IOException 
+     * @throws IOException
      */
-	@Test
-	public void testCargaCorrectaPrimerExploradorDesdeArchivo()throws FileNotFoundException, IOException{
-			ExploracionMarte exploracionMarte=ExploracionMarte.getInstancia(); 
-			Integer filas, columnas;
-			exploracionMarte.leerMundo();
-                        IExplorador explorador1 = exploracionMarte.getExplorador(1);
-                        Assert.assertTrue(explorador1.getXPos()==1);
-                        Assert.assertTrue(explorador1.getYPos()==2);
-                        Assert.assertTrue(explorador1.getDireccion().equals("N"));
-	}
+    @Test
+    public void testCargaCorrectaPrimerExploradorDesdeArchivo() throws FileNotFoundException, IOException {
+        ExploracionMarte exploracionMarte = ExploracionMarte.getInstancia();
+        Integer filas, columnas;
+        exploracionMarte.leerMundo();
+        IExplorador explorador1 = exploracionMarte.getExplorador(1);
+        Assert.assertTrue(explorador1.getXPos() == 1);
+        Assert.assertTrue(explorador1.getYPos() == 2);
+        Assert.assertTrue(explorador1.getDireccion().equals("N"));
+    }
+
+    @Test
+    public void girosExplorador() {
+        ExploracionMarte exploracionMarte = ExploracionMarte.getInstancia();
+        try {
+            exploracionMarte.leerMundo();
+        } catch (IOException ex) {
+            Logger.getLogger(TestControlArchivo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Explorador explorador = (Explorador) exploracionMarte.getExplorador(1);
+        explorador.girarDerecha();
+
+        Assert.assertEquals(explorador.getDireccion(), Explorador.DIRECCION_ESTE);
+
+    }
+
 }
